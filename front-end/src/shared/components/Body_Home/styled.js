500fe0cbc1846d6ea04f8783b1ad4b90595824a6
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-23 15:43:20 -0400

    :sparkles: feat: Compon. Body Home

---
+++ front-end/src/shared/components/Body_Home/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container_Contente = styled.section`
+  background-color: #ffffff;
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 0.8rem;
+
+  &&:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
