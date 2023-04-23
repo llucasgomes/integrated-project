3b1b673573ff131ae2c819069db7d3a4b319e670
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-23 15:39:29 -0400

    :sparkles: feat: Componente "Body Dasboard"

---
+++ front-end/src/shared/components/Body_Dasboard/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container_Body_Dashboard = styled.section`
+  background-color: #ffffff;
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  /* background-color: red; */
+  font-size: 2rem;
+  transition: 0.3s;
+`;
