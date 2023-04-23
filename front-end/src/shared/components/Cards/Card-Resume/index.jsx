3f3e2fce3c834e3846c28dbba7bcdaaac09edf2b
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-23 15:46:07 -0400

    :sparkles: feat: Compon. Cards > Card Resume

---
+++ front-end/src/shared/components/Cards/Card-Resume/index.jsx
@@ -0,0 +1,11 @@
+import { Container_Card_Resume } from "./styled";
+
+export const Card_Resume = ({ empresa, ano_inicio, ano_atual, descricao }) => {
+  return (
+    <Container_Card_Resume>
+      <h2 className="titulo">{empresa}</h2>
+      <h3 className="sub-titulo">{ano_inicio} - Present</h3>
+      <p className="descricao">{descricao}</p>
+    </Container_Card_Resume>
+  );
+};
---
+++ front-end/src/shared/components/Cards/Card-Resume/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container_Card_Resume = styled.div`
+  padding: 0.5rem;
+  max-width: 28rem;
+
+  .titulo {
+    font-size: 1.2rem;
+  }
+
+  .sub-titulo {
+    font-size: 0.9rem;
+  }
+
+  .descricao {
+    font-size: 0.8rem;
+  }
+`;
