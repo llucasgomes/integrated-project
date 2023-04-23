d4fe36e48b8d58d7922cb4215053e6383517bcad
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-23 15:37:53 -0400

    :sparkles: feat: Pagina de Dasboard

---
+++ front-end/src/pages/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import { Rotas_Dasboard } from "../../routes";
+import { Sidebar } from "../../shared/components/Sidebar";
+import { Container_Dasboard } from "./styled";
+
+export const Page_Dashboard = () => {
+  return (
+    <Container_Dasboard>
+      <Rotas_Dasboard />
+    </Container_Dasboard>
+  );
+};
---
+++ front-end/src/pages/Dashboard/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container_Dasboard = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
