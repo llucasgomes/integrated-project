5538f683b36f8efc0789dfc7e1860a835d394792
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-23 15:41:00 -0400

    :sparkles: feat: Compon. BDasboard > Boas Vindas

---
+++ front-end/src/shared/components/Body_Dasboard/BoasVindas/index.jsx
@@ -0,0 +1,13 @@
+import { Container_Bem_Vindo } from "./styled";
+
+export const Bem_Vindo = () => {
+  return (
+    <Container_Bem_Vindo>
+      <img
+        src="https://gifs.eco.br/wp-content/uploads/2022/06/gifs-de-sejam-bem-vindos-53.gif"
+        alt="imagem de bem vindo"
+      />
+      <h1>Mestre</h1>
+    </Container_Bem_Vindo>
+  );
+};
