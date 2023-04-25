6f3cb48e4054cf46b198f08826715e80f15f9f4e
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-25 10:44:27 -0400

    :recycle: refactor: Add "Outlet"

--- front-end/src/shared/components/Body_Dasboard/index.jsx
+++ front-end/src/shared/components/Body_Dasboard/index.jsx
@@ -1,26 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Container_Body_Dashboard } from "./styled";
-import { D_Perfil } from "./perfil";
-import { Bem_Vindo } from "./BoasVindas";
-import { Experiencia } from "./experiencias";
-import { Education } from "./Education";
-import { Projects } from "./Projetos";
-import { Habilidades } from "./Habilidades";
-import { Comments } from "./Comments";
 
 export const Body_Dashboard = () => {
   return (
     <Container_Body_Dashboard>
-      <Routes>
-        <Route path={"/"} element={<Bem_Vindo />} />
-        <Route path={"/perfil"} element={<D_Perfil />} />
-        <Route path={"/experiencias"} element={<Experiencia />} />
-        <Route path={"/educacao"} element={<Education />} />
-        <Route path={"/projetos"} element={<Projects />} />
-        <Route path={"/habilidades"} element={<Habilidades />} />
-        <Route path={"/comentarios"} element={<Comments />} />
-        <Route path={"/sobre"} element={"Sobre"} />
-      </Routes>
+      <Outlet />
     </Container_Body_Dashboard>
   );
 };
--- front-end/src/shared/components/Body_Home/index.jsx
+++ front-end/src/shared/components/Body_Home/index.jsx
@@ -1,21 +1,11 @@
-import { Route, Routes } from "react-router-dom";
 import { Container_Contente } from "./styled";
-import { Resume } from "./Resume";
-import { Habilidades } from "./Habilidades";
-import { Portifolio } from "./Portifolio";
-import { Sobre } from "./Sobre";
-import { Contato } from "./Contato";
+
+import { Outlet } from "react-router-dom";
 
 export const Contente = () => {
   return (
     <Container_Contente>
-      <Routes>
-        <Route path={"/"} element={<Resume />} />
-        <Route path={"/portifolio"} element={<Portifolio />} />
-        <Route path={"/habilidades"} element={<Habilidades />} />
-        <Route path={"/sobre"} element={<Sobre />} />
-        <Route path={"/contato"} element={<Contato />} />
-      </Routes>
+      <Outlet />
     </Container_Contente>
   );
 };
