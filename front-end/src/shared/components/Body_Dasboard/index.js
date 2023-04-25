9c85025a75d71338c49353d49c81db6afcc9ccc8
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-25 10:41:37 -0400

    :sparkles: feat: Delegando Path de componentes

---
+++ front-end/src/shared/components/Body_Dasboard/index.js
@@ -0,0 +1,7 @@
+export * from "./BoasVindas";
+export * from "./Comments";
+export * from "./Education";
+export * from "./Habilidades";
+export * from "./Projetos";
+export * from "./experiencias";
+export * from "./perfil";
---
+++ front-end/src/shared/components/Body_Home/index.js
@@ -0,0 +1,5 @@
+export * from "./Contato";
+export * from "./Habilidades";
+export * from "./Portifolio";
+export * from "./Resume";
+export * from "./Sobre";
