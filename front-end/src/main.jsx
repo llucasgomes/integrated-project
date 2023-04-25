1d0ce58f327d71b7ce3d5782ee712c2d65d812fe
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-25 10:50:05 -0400

    :recycle: feat: Usando o "RouterProvider"

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
+
 import "./index.css";
 import GlobalStyle from "./styles/Global";
 import background_image from "./assets/images/bg.png";
 
-import { ProSidebarProvider } from "react-pro-sidebar";
+import { RouterProvider } from "react-router-dom";
+import { Rota } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ProSidebarProvider>
-      <App />
-    </ProSidebarProvider>
+    <RouterProvider router={Rota} />
     <GlobalStyle image={background_image} />
   </React.StrictMode>
 );
