e27205f5aaa8f35e503dcf8450fed1eb4f8ea68f
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-25 23:47:08 -0400

    :sparkles: feat: Modal Projetos

---
+++ front-end/src/shared/components/Modals/Modal-Projetos/index.jsx
@@ -0,0 +1,105 @@
+import { Input } from "../../Input";
+import { Container_Modal, Row } from "./styled";
+
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+
+export const Modal_Projetos = ({ setOpenModal }) => {
+  // Valores iniciais Formik
+  const initialValues = {
+    title: "",
+    descricao: "",
+    github: "",
+    page: "",
+  };
+  //Validações YUP
+  const validationSchema = Yup.object({
+    title: Yup.string()
+      .min(3, "O campo deve ter no mínimo 3 caracteres")
+      .required("Campo obrigatório"),
+    descricao: Yup.string()
+      .min(3, "O campo deve ter no mínimo 3 caracteres")
+      .max(70, "O campo deve ter no maximo 150 caracteres")
+      .required("Campo obrigatório"),
+    github: Yup.string()
+      .min(3, "O campo deve ter no mínimo 3 caracteres")
+      .required("Campo obrigatório"),
+    page: Yup.string()
+      .min(3, "O campo deve ter no mínimo 3 caracteres")
+      .required("Campo obrigatório"),
+  });
+
+  //Notify
+  const notify = () =>
+    toast.success("Email enviado com Sucesso!", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+
+  //Envio do formulario
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    // notify();
+    // resetForm();
+    setSubmitting(false);
+  };
+  return (
+    <Container_Modal>
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        {/* <div className="title">
+          <h1>😜 Inserir</h1>
+        </div> */}
+        <div className="body">
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+            enableReinitialize
+          >
+            <Form>
+              <Row>
+                <img
+                  src="https://miro.medium.com/v2/resize:fit:1400/1*g09N-jl7JtVjVZGcd-vL2g.jpeg"
+                  alt=""
+                />
+                <Row flex={""}>
+                  <Input name="title" required />
+                  <Input name="gitHub" required />
+                  <Input name="page" required />
+                </Row>
+              </Row>
+              <Row height={"textArea"}>
+                <Input name="descricao" type="text" required />
+              </Row>
+            </Form>
+          </Formik>
+        </div>
+        <div className="footer">
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+            id="cancelBtn"
+          >
+            Cancel
+          </button>
+          <button>Continue</button>
+        </div>
+      </div>
+    </Container_Modal>
+  );
+};
