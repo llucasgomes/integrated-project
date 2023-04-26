e50b4a8f4603ceea59bd37e90f533f87d83cc144
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-25 23:51:35 -0400

    :truck: Renomeado Contente > Conteudo

--- front-end/src/shared/components/Body_Home/index.jsx
+++ front-end/src/shared/components/Body_Home/index.jsx
@@ -2,7 +2,7 @@ import { Container_Contente } from "./styled";
 
 import { Outlet } from "react-router-dom";
 
-export const Contente = () => {
+export const Conteudo = () => {
   return (
     <Container_Contente>
       <Outlet />
