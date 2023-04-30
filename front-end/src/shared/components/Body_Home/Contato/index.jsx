771439e67e526e4d6ff4bbf6908a5a75ef818975
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-04-30 19:47:27 -0400

    :sparkles: feat: Section de "Redes Sociais"

--- front-end/src/shared/components/Body_Home/Contato/index.jsx
+++ front-end/src/shared/components/Body_Home/Contato/index.jsx
@@ -1,6 +1,14 @@
 import { Form_Email } from "../../Form-Email";
 import { Mapa } from "../../Mapa";
 import { Container_Contato } from "./styled";
+import {
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  WhatsappLogo,
+} from "phosphor-react";
+
+import { Link } from "react-router-dom";
 
 export const Contato = () => {
   return (
@@ -9,7 +17,37 @@ export const Contato = () => {
         <Form_Email />
 
         <div className="info">
-          <h2>Section right</h2>
+          <Link
+            to={"https://www.google.com.br"}
+            className="cartao_link"
+            target="_blank"
+          >
+            <GithubLogo size={56} weight="regular" />
+          </Link>
+
+          <Link
+            className="cartao_link"
+            target="_blank"
+            to={"https://www.google.com.br"}
+          >
+            <LinkedinLogo size={56} weight="regular" />
+          </Link>
+
+          <Link
+            className="cartao_link"
+            target="_blank"
+            to={"https://www.google.com.br"}
+          >
+            <WhatsappLogo size={56} weight="regular" />
+          </Link>
+
+          <Link
+            className="cartao_link"
+            target="_blank"
+            to={"https://www.google.com.br"}
+          >
+            <InstagramLogo size={56} weight="regular" />
+          </Link>
         </div>
       </div>
       <Mapa />
--- front-end/src/shared/components/Body_Home/Contato/styled.js
+++ front-end/src/shared/components/Body_Home/Contato/styled.js
@@ -11,13 +11,33 @@ export const Container_Contato = styled.section`
     .form {
       width: 50%;
       height: 300px;
-      background-color: red;
     }
     .info {
+      display: flex;
+      flex-wrap: wrap;
       width: 50%;
       height: 50%;
-      background-color: blue;
+      gap: 10px;
       height: 300px;
+
+      .cartao_link {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: #363547;
+
+        width: 45%;
+        height: 50%;
+
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        transition: 0.3s;
+        border-radius: 0.8rem;
+
+        &:hover {
+          box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+          color: #000000;
+        }
+      }
     }
   }
 `;
