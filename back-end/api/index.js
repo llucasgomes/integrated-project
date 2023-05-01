dbba6915cda9cb21a66848580cce7b315d42db58
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-01 17:14:40 -0400

    :recycle::construction: chore: Todo o codigo

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -7,273 +7,294 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: "db4free.net",
-    user: "llucasgomes",
-    password: "LhwSd0VXNbVjxkXY",
-    database: "portiflio",
-    port: 3306
+  host: "db4free.net",
+  user: "llucasgomes",
+  password: "_*W5DvSvT2$mvmW",
+  database: "portiflio",
+  port: 3306,
 });
 
 app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Sucesso!"
-    });
+  res.json({
+    success: true,
+    message: "Sucesso!",
+  });
 });
 
-
-//  ============== GETS ===================
-// GET SKILLS ===========================
-app.get("/skills", (req, res) => {
-    const q = "SELECT * FROM skills";
-    db.query(q, (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.json(err);
-        }
-        return res.json(data);
-    });
-});
-
-// GET USER =============================
-app.get("/users", (req, res) => {
-    const q = "SELECT * FROM users";
-    db.query(q, (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.json(err);
-        }
-        return res.json(data);
-    });
-});
-// GET PROJECTS =========================
-app.get("/projects", (req, res) => {
-    const q = "SELECT * FROM projects";
-    db.query(q, (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.json(err);
-        }
-        return res.json(data);
-    });
-});
-// GET SOCIAL NETWORKS ==================
-app.get("/social_networks", (req, res) => {
-    const q = "SELECT * FROM social_networks";
-    db.query(q, (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.json(err);
-        }
-        return res.json(data);
-    });
-});
-// GET PROFILE ==========================
+// MOSTRAR PROFILE ===========================
 app.get("/profile", (req, res) => {
-    const q = "SELECT * FROM profile";
-    db.query(q, (err, data) => {
-        if (err) {
-            console.log(err);
-            return res.json(err);
-        }
-        return res.json(data);
-    });
+  const q = "SELECT * FROM profile";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
 });
 
+// CRIAR PROFILE ==========================
+app.post("/profile", (req, res) => {
+  const q =
+    "INSERT INTO profile(`image`, `name`, `middle_name`, `last_name`, `birthday`, `job`, `phone`, `email`) VALUES (?)";
 
-//  ============== CREATE ===================
-// POST SKILLS ==========================
-app.post("/skills", (req, res) => {
-    const q = "INSERT INTO skills(`title`, `desc`, `imagem`, `studying`) VALUES (?)";
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.middle_name,
+    req.body.last_name,
+    req.body.birthday,
+    req.body.job,
+    req.body.phone,
+    req.body.email,
+  ];
 
-    const values = [
-        req.body.title,
-        req.body.desc,
-        req.body.imagem,
-        req.body.studying,
-    ];
-
-    db.query(q, [values], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
 });
-// POST PROJECTS ==========================
-app.post("/projects", (req, res) => {
-    const q = "INSERT INTO projects(`name`, `link`, `image`) VALUES (?)";
-
-    const values = [
-        req.body.name,
-        req.body.link,
-        req.body.image,
-    ];
-
-    db.query(q, [values], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-// POST USERS ==========================
-app.post("/user", (req, res) => {
-    const q = "INSERT INTO users(`name`, `img_profile`, `level`) VALUES (?)";
-
-    const values = [
-        req.body.name,
-        req.body.img_profile,
-        req.body.level,
-    ];
-
-    db.query(q, [values], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-// POST SOCIAL NETWORKS ==========================
-app.post("/social_networks", (req, res) => {
-    const q = "INSERT INTO social_networks(`name`, `link`, `image`) VALUES (?)";
-
-    const values = [
-        req.body.name,
-        req.body.link,
-        req.body.image,
-    ];
-
-    db.query(q, [values], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-
-//  ============== UPDATE ===================
-// PUT PROJECTS ==========================
-app.put("/projects/:id", (req, res) => {
-    const projectId = req.params.id;
-    const q = "UPDATE projects SET `name`= ?, `link`= ?, `image`= ? WHERE id = ?";
-
-    const values = [
-        req.body.name,
-        req.body.link,
-        req.body.image,
-    ];
-
-    db.query(q, [...values, projectId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-// PUT USER ==========================
-app.put("/user/:id", (req, res) => {
-    const userId = req.params.id;
-    const q = "UPDATE users SET `name`= ?, `img_profile`= ?, `level`= ? WHERE id = ?";
-
-    const values = [
-        req.body.name,
-        req.body.img_profile,
-        req.body.level,
-    ];
-
-    db.query(q, [...values, userId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-// PUT PROFILE ==========================
+// EDITAR PROFILE ==========================
 app.put("/profile/:id", (req, res) => {
-    const profileId = req.params.id;
-    const q = "UPDATE profile SET `name`= ?, `about`= ? WHERE id = ?";
+  const profileId = req.params.id;
+  const q =
+    "UPDATE profile SET `image`= ?, `name`= ?, `middle_name`= ?, `last_name`= ?, `birthday`= ?, `job`= ?, `phone`= ?, `email`= ? WHERE id = ?";
 
-    const values = [
-        req.body.name,
-        req.body.about
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.middle_name,
+    req.body.last_name,
+    req.body.birthday,
+    req.body.job,
+    req.body.phone,
+    req.body.email,
+  ];
 
-    ];
-
-    db.query(q, [...values, profileId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-// PUT SOCIAL NETWORKS ==========================
-app.put("/social_networks/:id", (req, res) => {
-    const social_networkId = req.params.id;
-    const q = "UPDATE social_networks SET `name`= ?, `link`= ?, `image`= ? WHERE id = ?";
-
-    const values = [
-        req.body.name,
-        req.body.link,
-        req.body.image,
-    ];
-
-    db.query(q, [...values, social_networkId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-// PUT SKILLS ==========================
-app.put("/skills/:id", (req, res) => {
-    const skillsId = req.params.id;
-    const q = "UPDATE skills SET `title`= ?, `desc`= ?, `image`= ?, `studying`= ? WHERE id = ?";
-
-    const values = [
-        req.body.title,
-        req.body.desc,
-        req.body.imagem,
-        req.body.studying,
-
-    ];
-
-    db.query(q, [...values, skillsId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
+  db.query(q, [...values, profileId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
 });
 
-//  ============== DELETE ===================
-// DELETE SKILLS ==========================
-app.delete("/skills/:id", (req, res) => {
-    const skillsId = req.params.id;
-    const q = " DELETE FROM skills WHERE id = ? ";
+// DELETE PROFILE ==========================
+app.delete("/profile/:id", (req, res) => {
+  const usersId = req.params.id;
+  const q = " DELETE FROM profile WHERE image = ? ";
 
-    db.query(q, [skillsId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
-});
-// DELETE PROJECTS ==========================
-app.delete("/projects/:id", (req, res) => {
-    const projectsId = req.params.id;
-    const q = " DELETE FROM projects WHERE id = ? ";
-
-    db.query(q, [projectsId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
+  db.query(q, [usersId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
 });
 
-// DELETE SOCIAL NETWORKS ==========================
-app.delete("/social_networks/:id", (req, res) => {
-    const social_networksId = req.params.id;
-    const q = " DELETE FROM social_networks WHERE id = ? ";
+// =========================================================================
 
-    db.query(q, [social_networksId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
+// CRIAR HABILIDADE ==========================
+app.post("/skills", (req, res) => {
+  const q = "INSERT INTO skills(`image`, `title`, `description`) VALUES (?)";
+
+  const values = [req.body.image, req.body.title, req.body.description];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
 });
 
-// DELETE USERS ==========================
-app.delete("/users/:id", (req, res) => {
-    const usersId = req.params.id;
-    const q = " DELETE FROM users WHERE id = ? ";
-
-    db.query(q, [usersId], (err, data) => {
-        if (err) return res.send(err);
-        return res.json(data);
-    });
+// MOSTRAR HABILIDADES ===========================
+app.get("/skills", (req, res) => {
+  const q = "SELECT * FROM skills";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
 });
 
-const port = process.env.PORT || 4000
+// =========================================================================
+// CRIAR EDUCATION ===========================
+app.post("/education", (req, res) => {
+  const q =
+    "INSERT INTO education (`course`,`start_date`,`end_date`,`description`) VALUES (?)";
+  const values = [
+    req.body.course,
+    req.body.start_date,
+    req.body.end_date,
+    req.body.description,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
 
-app.listen(port, () => {
-    console.log('Server running in ' + port)
-})
+// MOSTRAR EDUCATION ===========================
+app.get("/education", (req, res) => {
+  const q = "SELECT * FROM education";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+// EDITAR EDUCATION ==========================
+app.put("/education/:id", (req, res) => {
+  const educationId = req.params.id;
+  const q =
+    "UPDATE education SET `course`= ?, `start_date`= ?, `end_date`= ?, `description`= ? WHERE id = ?";
+
+  const values = [
+    req.body.course,
+    req.body.start_date,
+    req.body.end_date,
+    req.body.description,
+  ];
+
+  db.query(q, [...values, educationId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+// DELETE EDUCATION ==========================
+app.delete("/education/:id", (req, res) => {
+  const educationId = req.params.id;
+  const q = " DELETE FROM education WHERE id = ? ";
+
+  db.query(q, [educationId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+// =========================================================================
+// CRIAR EXPERIENCE ===========================
+app.post("/experience", (req, res) => {
+  const q =
+    "INSERT INTO experience (`company`,`office`,`start_date`,`end_date`,`description`) VALUES (?)";
+  const values = [
+    req.body.company,
+    req.body.office,
+    req.body.start_date,
+    req.body.end_date,
+    req.body.description,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+// MOSTRAR EXPERIENCE ===========================
+app.get("/experience", (req, res) => {
+  const q = "SELECT * FROM experience";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+// EDITAR EXPERIENCE ==========================
+app.put("/experience/:id", (req, res) => {
+  const educationId = req.params.id;
+  const q =
+    "UPDATE experience SET `company`= ?,`office`= ?, `start_date`= ?, `end_date`= ?, `description`= ? WHERE id = ?";
+
+  const values = [
+    req.body.company,
+    req.body.office,
+    req.body.start_date,
+    req.body.end_date,
+    req.body.description,
+  ];
+
+  db.query(q, [...values, educationId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+// DELETE EXPERIENCE ==========================
+app.delete("/experience/:id", (req, res) => {
+  const experienceId = req.params.id;
+  const q = " DELETE FROM experience WHERE id = ? ";
+
+  db.query(q, [experienceId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+// =========================================================================
+// CRIAR PROJECT ===========================
+app.post("/project", (req, res) => {
+  const q =
+    "INSERT INTO projects (`image`,`title`,`description`,`github`,`page`) VALUES (?)";
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.description,
+    req.body.github,
+    req.body.page,
+  ];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+// MOSTRAR PROJECT ===========================
+app.get("/project", (req, res) => {
+  const q = "SELECT * FROM projects";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+// EDITAR PROJECT ==========================
+app.put("/project/:id", (req, res) => {
+  const projectId = req.params.id;
+  const q =
+    "UPDATE projects SET `image`= ?,`title`= ?, `description`= ?, `github`= ?, `page`= ? WHERE id = ?";
+
+  const values = [
+    req.body.image,
+    req.body.title,
+    req.body.description,
+    req.body.github,
+    req.body.page,
+  ];
+
+  db.query(q, [...values, projectId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+// DELETE PROJECT ==========================
+app.delete("/project/:id", (req, res) => {
+  const projectId = req.params.id;
+  const q = " DELETE FROM projects WHERE id = ? ";
+
+  db.query(q, [projectId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+
+
+app.listen(3000);
