18ce1d13ba9c571189899a631a31947e43fe6445
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-04 22:06:28 -0400

    :sparkles: feat: Add "DataContextProvider"

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,10 +7,13 @@ import background_image from "./assets/images/bg.png";
 
 import { RouterProvider } from "react-router-dom";
 import { Rota } from "./routes";
+import { DataContextProvider } from "./shared/contexts/DataContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={Rota} />
-    <GlobalStyle image={background_image} />
+    <DataContextProvider>
+      <RouterProvider router={Rota} />
+      <GlobalStyle image={background_image} />
+    </DataContextProvider>
   </React.StrictMode>
 );
