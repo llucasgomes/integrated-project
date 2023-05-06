54cc536765d4a68049aa80ed19d18a0850405a27
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-06 06:45:27 -0400

    :sparkles: feat: Add "Axios > POST"

--- front-end/src/shared/components/Modals/Modal-Experiencia/index.jsx
+++ front-end/src/shared/components/Modals/Modal-Experiencia/index.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+import { API } from "../../../services/api";
 import { Input } from "../../Input";
 import { Container_Modal, Row } from "./styled";
 
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
+import { DataContext } from "../../../contexts/DataContext";
+import { newData } from "../../../functions";
 
 export const Modal_Experiencia = ({ setOpenModal }) => {
+  const { setIsFetching } = useContext(DataContext);
+
   // Valores iniciais Formik
   const initialValues = {
     empresa: "",
@@ -19,9 +25,6 @@ export const Modal_Experiencia = ({ setOpenModal }) => {
       .min(3, "O campo deve ter no mínimo 3 caracteres")
       .required("Campo obrigatório"),
     inicio: Yup.date()
-      .max(new Date(), "Não é possível incluir uma data futura")
-      .required("Campo obrigatório"),
-    termino: Yup.date()
       .max(new Date(), "Não é possível incluir uma data futura")
       .required("Campo obrigatório"),
     descricao: Yup.string()
@@ -48,8 +51,21 @@ export const Modal_Experiencia = ({ setOpenModal }) => {
 
   //Envio do formulario
   const handleSubmit = (values, { setSubmitting, resetForm }) => {
-    // notify();
-    // resetForm();
+    API.post("/experience", {
+      company: values.empresa,
+      office: values.cargo,
+      start_date: newData(values.inicio),
+      end_date: values.termino ? newData(values.termino0) : "Atual",
+      description: values.descricao,
+    })
+      .then((res) => {
+        console.log(res);
+        setIsFetching(true);
+        setOpenModal(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
     setSubmitting(false);
   };
   return (
@@ -75,31 +91,35 @@ export const Modal_Experiencia = ({ setOpenModal }) => {
             onSubmit={handleSubmit}
             enableReinitialize
           >
-            <Form>
-              <Row>
-                <Input name="empresa" required />
-              </Row>
-              <Row>
-                <Input name="cargo" required />
-                <Input name="inicio" type="date" required />
-                <Input name="termino" type="date" required />
-              </Row>
-              <Row height={"textArea"}>
-                <Input name="descricao" type="text" required />
-              </Row>
-            </Form>
+            {({ values, isSubmitting }) => (
+              <Form>
+                <Row>
+                  <Input name="empresa" required />
+                </Row>
+                <Row>
+                  <Input name="cargo" required />
+                  <Input name="inicio" type="date" required />
+                  <Input name="termino" type="date" required />
+                </Row>
+                <Row height={"textArea"}>
+                  <Input name="descricao" type="text" required />
+                </Row>
+                <div className="footer">
+                  <button
+                    onClick={() => {
+                      setOpenModal(false);
+                    }}
+                    id="cancelBtn"
+                  >
+                    Cancel
+                  </button>
+                  <button type="submit" disabled={isSubmitting}>
+                    Salvar
+                  </button>
+                </div>
+              </Form>
+            )}
           </Formik>
-        </div>
-        <div className="footer">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            id="cancelBtn"
-          >
-            Cancel
-          </button>
-          <button>Continue</button>
         </div>
       </div>
     </Container_Modal>
