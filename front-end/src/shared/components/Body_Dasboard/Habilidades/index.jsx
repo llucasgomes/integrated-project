7a1951eaf38b74c11027b4a8d07ebdab5cd4938b
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-06 05:34:43 -0400

    :sparkles: chore: Add T_Habilidade + Context

--- front-end/src/shared/components/Body_Dasboard/Habilidades/index.jsx
+++ front-end/src/shared/components/Body_Dasboard/Habilidades/index.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Tabela } from "../../Tabelas";
 import { Modal_Habilidades } from "../../Modals/Modal-Habilidades";
 import { Container_Habilidades } from "./styled";
+import { Table_Habilidade } from "../../Tabelas/Habilidades";
 
 export const D_Habilidades = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
 
   return (
     <Container_Habilidades>
@@ -18,18 +19,17 @@ export const D_Habilidades = () => {
           adicionar
         </button>
       </div>
-      <Tabela
-        titles={[
-          "ID",
-          "Instituição",
-          "Inicio",
-          "Termino",
-          "Descrição",
-          "Botoes",
-          "Botoes",
-        ]}
+      <Table_Habilidade
+        titles={["ID", "Imagem", "Title", "Descrição"]}
+        isFetching={setIsFetching}
+        setIsFetching={setIsFetching}
       />
-      {modalOpen && <Modal_Habilidades setOpenModal={setModalOpen} />}
+      {modalOpen && (
+        <Modal_Habilidades
+          setOpenModal={setModalOpen}
+          setIsFetching={setIsFetching}
+        />
+      )}
     </Container_Habilidades>
   );
 };
