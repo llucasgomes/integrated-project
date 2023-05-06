589b1ca85bdcc994016fcbc515921dc56b267453
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-06 06:49:50 -0400

    :sparkles: chore: Add if ternario

--- front-end/src/shared/components/Cards/Card-Resume/index.jsx
+++ front-end/src/shared/components/Cards/Card-Resume/index.jsx
@@ -1,9 +1,16 @@
 import { Container_Card_Resume } from "./styled";
 
-export const Card_Resume = ({ empresa, ano_inicio, ano_atual, descricao }) => {
+export const Card_Resume = ({
+  empresa,
+  ano_inicio,
+  ano_atual,
+  descricao,
+  cargo,
+}) => {
   return (
     <Container_Card_Resume>
       <h2 className="titulo">{empresa}</h2>
+      {cargo ? <h3 className="sub-titulo">{cargo}</h3> : null}
       <h3 className="sub-titulo">
         {ano_inicio} - {ano_atual}
       </h3>
