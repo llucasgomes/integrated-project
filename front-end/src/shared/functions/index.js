b1a79026905eefd56662db70e9f0cae1ea4d845c
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-06 06:50:44 -0400

    :recycle: refactor: Retirando Comentario

--- front-end/src/shared/functions/index.js
+++ front-end/src/shared/functions/index.js
@@ -1,5 +1,5 @@
 export const newData = (data) => {
-    let date = data.slice(0, 10);
+  let date = data.slice(0, 10);
 
-    return (date = date.split("-").reverse().join("-"));
-  };
+  return (date = date.split("-").reverse().join("-"));
+};
