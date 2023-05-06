1554f36bec5a986ab3d89ce293db3e4bd59e68bd
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-06 06:53:17 -0400

    :sparkles: feat: Add Tabela Experiencia

--- front-end/src/shared/components/Body_Dasboard/experiencias/index.jsx
+++ front-end/src/shared/components/Body_Dasboard/experiencias/index.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { Tabela } from "../../Tabelas";
 import { Container_Experincia } from "./styled";
 import { Modal_Experiencia } from "../../Modals/Modal-Experiencia";
+import { Table_Experience } from "../../Tabelas/Experience";
 
 export const Experiencia = () => {
   const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <Container_Experincia>
-      <h2 className="title">Experiencias</h2>
+      <h2 className="title">Experiências</h2>
       <div>
         <button
           onClick={() => {
@@ -18,16 +19,8 @@ export const Experiencia = () => {
           adicionar
         </button>
       </div>
-      <Tabela
-        titles={[
-          "ID",
-          "Instituição",
-          "Inicio",
-          "Termino",
-          "Descrição",
-          "Botoes",
-          "Botoes",
-        ]}
+      <Table_Experience
+        titles={["ID", "Empresa", "Cargo", "Inicio", "Termino", "Descrição"]}
       />
       {modalOpen && <Modal_Experiencia setOpenModal={setModalOpen} />}
     </Container_Experincia>
