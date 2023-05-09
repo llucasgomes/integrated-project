a86f270988600379d3f3e271aa00dd50f26792fd
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-08 22:46:28 -0400

    :sparkles: feat: Add "Loading"

--- front-end/src/pages/Home/styled.js
+++ front-end/src/pages/Home/styled.js
@@ -19,3 +19,12 @@ export const Container_Home = styled.div`
     gap: 1.5rem;
   }
 `;
+
+export const Container_Loading = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #f5f5f5;
+`;
