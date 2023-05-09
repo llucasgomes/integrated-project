20055a7a685185007cd56b0569bf560151a34b80
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-05-08 22:45:58 -0400

    :sparkles: feat: Add "loading"

--- front-end/src/pages/Home/index.jsx
+++ front-end/src/pages/Home/index.jsx
@@ -1,21 +1,41 @@
 import { Profile_Section } from "../../shared/components/Profile";
-import { Container_Home } from "./styled";
+import { Container_Home, Container_Loading } from "./styled";
 import { Navbar } from "../../shared/components/Navbar";
 import { Conteudo } from "../../shared/components/Body_Home/index.jsx";
+import { DataContext } from "../../shared/contexts/DataContext";
+import { useContext } from "react";
+
+import ReactLoading from "react-loading";
 
 export const Home = () => {
+  const { isFetching } = useContext(DataContext);
   return (
     <Container_Home>
-      {/* Seção da Esquerda */}
-      <Profile_Section />
-
-      {/* Seção da Direita */}
-      <div className="container">
+      {isFetching ? (
         <>
-          <Navbar />
-          <Conteudo />
+          <Container_Loading>
+            <ReactLoading
+              type={"spin"}
+              color={"#7279f9"}
+              height={100}
+              width={100}
+            />
+          </Container_Loading>
         </>
-      </div>
+      ) : (
+        <>
+          {/* Seção da Esquerda */}
+          <Profile_Section />
+
+          {/* Seção da Direita */}
+          <div className="container">
+            <>
+              <Navbar />
+              <Conteudo />
+            </>
+          </div>
+        </>
+      )}
     </Container_Home>
   );
 };
